014f62202915095fa935a92fc499f6cc20ea1a3b
Test Git <git@publicplan.de>
2024-01-18 12:15:00 +0100

    Pug template used

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 // Middleware to handle favicon request
 app.use('/favicon.ico', (req, res) => res.status(204));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page not found'});
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', {prods: products, pageTitle: 'My Shop', path: '/'});
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
