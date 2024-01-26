baa9a5620ba5e12294ae3348b251c002ae58a052
Test Git <git@publicplan.de>
2024-01-26 15:37:08 +0100

    cart model - add and remove items

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getIndex = (req, res, index) => {
     Product.fetchAll(products => {
@@ -20,10 +21,49 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        res.render('shop/product-detail', {
+            product: product, 
+            pageTitle: 'Product -' + product.title, 
+            path: '/products'
+        });
+    });
+};
+
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart'
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            for (product of products) {
+                const cartProduct = cart.products.find(prod => prod.id === product.id);
+                if (cartProduct) {
+                    cartProducts.push({productData: product, qty: cartProduct.qty});
+                }
+            }
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                cart: cartProducts
+            });
+        });
+    })
+};
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, (product)=> {
+        Cart.addProduct(prodId, product.price);
+    });
+    res.redirect('/cart');
+};
+
+exports.postDeleteCartItem = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+        Cart.deleteProduct(prodId, product.price);
+        res.redirect('/cart');
     });
 };
 
